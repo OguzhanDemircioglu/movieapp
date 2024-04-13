a432d611b6a5fde1584ec5fafb3dcb555f7aabfe
Oğuzhan <oguzhanturgut611@gmail.com>
2024-04-13 16:26:35 +0300

    first

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+categories =["macera", "romantik", "dram","bilim kurgu"]
+
+def home(request):
+  data = {
+    "categories" : categories
+  }
+  return render(request, 'index.html',data)
+
+def movies(request):
+  return render(request, 'movies.html')
