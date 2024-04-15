bab9b90ac86d5540442a9324f1a2eeaed89f4eaf
Oğuzhan <oguzhanturgut611@gmail.com>
2024-04-15 03:03:45 +0300

    home, movies section

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 
-categories =["macera", "romantik", "dram","bilim kurgu"]
+categories = ["macera", "romantik", "dram", "bilim kurgu"]
+films = [
+  {
+    "title": "Flim1",
+    "description": "Flim1 description",
+    "image": "1.jpg",
+    "anasayfa": True
+  },
+  {
+    "title": "Flim2",
+    "description": "Flim2 description",
+    "image": "2.jpg",
+    "anasayfa": True
+  },
+  {
+    "title": "Flim3",
+    "description": "Flim3 description",
+    "image": "3.jpg",
+    "anasayfa": False
+  }
+]
 
 def home(request):
   data = {
-    "categories" : categories
+    "categories": categories,
+    "films": films
   }
-  return render(request, 'index.html',data)
+  return render(request, 'index.html', data)
 
 def movies(request):
-  return render(request, 'movies.html')
+  data = {
+    "categories": categories,
+    "films": films
+  }
+  return render(request, 'movies.html',data)
